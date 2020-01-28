3d8ec0a17965221d021058bb1bda5a2e97a1d510
German Cruz Moreno <cray.pro95@gmail.com>
2020-01-27 20:35:54 -0600

    Separado por Batches

--- index.js
+++ index.js
@@ -3,87 +3,165 @@ const fs = require("fs")
 const https = require('https');
 const csv = require('csvtojson')
 const headers = {
-    'X-Auth-Token': 'Go6282lo3c9fgguub6ra77f9hagdotn4',
+    'X-Auth-Token': '1k5umneoa52j9a7tntqrcgyqmlmmstgstlvgg6da7',
     'X-Auth-Tenant': 'Edulynks'
 };
-const pathInputCSV = 'input.csv' //Nombre del archivo input donde se espera que esten los ids
-const pathOutputCSV = 'output.csv' //Nombre del archivo output donde se guardara el resultado
-const opcionesParaExportarCSV = {
+const ENDPOINT_API_URL = `/masterkey/agency/sale/:id/saleOrder`
+const PATH_INPUT_CSV = 'input 2.csv' //Nombre del archivo input donde se espera que esten los ids
+const PATH_OUTPUT_CSV = 'output.csv' //Nombre del archivo output donde se guardara el resultado
+const EXPORT_CSV_OPTIONS = {
     "fields": [{
-        "name": "name",
-        "label": "Name"
-    }]
+            "name": "sale.id",
+            "label": "Sale id"
+        },
+        {
+            "name": "status",
+            "label": "Sale status"
+        },
+        {
+            "name": "institute.name",
+            "label": "institute name"
+        },
+        {
+            "name": "institute.city.name",
+            "label": "city"
+        },
+        {
+            "name": "institute.city.countryCode",
+            "label": "countryCode"
+        },
+        {
+            "name": "course.name",
+            "label": "course name"
+        },
+        {
+            "name": "courseLine.startDate",
+            "label": "course line startDate"
+        },
+        {
+            "name": "courseLine.endDate",
+            "label": "course line endDate"
+        },
+    ]
 }
-
+var waitingBatch = false;
+var finalSaleOrderItems = [];
 
 let main = async () => {
-    const ids = await getIdsFromCSV() //obtener los ids desde el input
-    let items = [];
-    count = ids.length;
-    ids.forEach((saleId) => {
-        const urlDelApi = `/masterkey/agency/sale/:id/saleOrder`
-        let url = urlDelApi;
-        url = url.replace(":id", saleId["field1"].toString());
+    const idsOfSaleFromCSV = await GetIdsFromCSV() //obtener los ids desde el input
+    // const ids = testIds() //obtener los ids desde el input
+    console.log("idsOfSaleFromCSV length", idsOfSaleFromCSV.length)
+    const STEP = 20
+    let first = 0
+    let last = STEP
+    do {
+        FinishBatchCallback(await CheckThisBatch(idsOfSaleFromCSV.slice(first, last), first, last));
+        first += STEP;
+        last += STEP;
 
-      
-        let getOptions ={
-            hostname: 'server.bookandlearn.com',
-            port: 443,
-            path: url,
-            method: 'GET',
-            headers
-          };
-        https.get(getOptions, (resp) => {
-            let data = '';
-            const {statusCode,headers} = resp;
-            console.log("Respuesta",statusCode,headers,resp);
-            // A chunk of data has been recieved.
-            resp.on('data', (chunk) => {
-                if (chunk != undefined) {
-                    data += chunk;
-                }
+    } while (finalSaleOrderItems.length != idsOfSaleFromCSV.length)
+    putResultInCSV() //Genera el CSV a partir de los items
+    console.log("Terminado"); //visualmente mostramos que termino el proceso
+
+}
+let CheckThisBatch = (idsOfSaleFromCSV, first, last) => {
+    return new Promise(resolve => {
+        ReturnedPetitionCounter = idsOfSaleFromCSV.length;
+
+        let batchItems = []
+        console.log(`Buscando items del array ${first} hasta ${last}`);
+        idsOfSaleFromCSV.forEach((saleId) => {
+            let url = ENDPOINT_API_URL;
+            url = url.replace(":id", saleId["field1"].toString());
+            // console.log(2,url);
+
+
+            let getOptions = {
+                hostname: 'server.bookandlearn.com',
+                port: 443,
+                path: url,
+                method: 'GET',
+                headers
+            };
+            //   console.log(3,getOptions);
+
+            https.get(getOptions, (resp) => {
+                let data = '';
+
+                const {
+                    statusCode,
+                    headers
+                } = resp;
+                // console.log("Respuesta",statusCode,headers,resp);
+                // A chunk of data has been recieved.
+                resp.on('data', (chunk) => {
+                    if (chunk != undefined) {
+                        data += chunk;
+                    }
+                });
+
+                // The whole response has been received. Print out the result.
+                resp.on('end', () => {
+                    // console.log(data)
+                    data = JSON.parse(data);
+                    ReturnedPetitionCounter--;
+                    // console.log(url, data, ReturnedPetitionCounter);
+                    if (data.sale == undefined) {
+                        data.sale = {};
+                        data.sale.id = saleId["field1"];
+                    }
+                    batchItems.push(data)
+
+                    if (HasFinishScanning(ReturnedPetitionCounter)) {
+                        resolve(batchItems);
+                        // console.log(`\rEncontrado items del array ${first} hasta ${last}`); //visualmente mostramos que termino el proceso
+                    }
+                });
+
+            }).on("error", (err) => {
+                console.log("Error: " + err.message);
             });
-
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-
-                data = JSON.parse(data);
-                count--;
-                console.log(url, data, count);
-                if (!data.hasOwnProperty("error")) {
-                    items.push(data);
-                }
-                if (count == 0) { //Si ya terminaron todos, lo añade al CSV
-                    items.push({
-                        "name": `Ids escaneados ${items.length}`
-                    }); //Al final del archivo output dice cuantos ids escaneo
-                    putResultInCSV(items) //Genera el CSV a partir de los items
-                    console.log("Terminado"); //visualmente mostramos que termino el proceso
-                }
-            });
-
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
-        });
+        })
     })
 }
+let FinishBatchCallback = (batchItems) => {
+    MergeObtainedItems(batchItems);
 
+}
+let MergeObtainedItems = (batchItems) => {
+    finalSaleOrderItems = finalSaleOrderItems.concat(batchItems);
+}
+let AskForAnotherBatch = () => {
+    waitingBatch = false;
+}
+let HasFinishScanning = (ReturnedPetitionCounter) => {
+    return ReturnedPetitionCounter == 0;
+}
 
-let putResultInCSV = async (items) => {
+let putResultInCSV = async () => {
 
-    jsoncsv.buffered(items, opcionesParaExportarCSV, (err, csv) => {
-        let out = fs.createWriteStream(pathOutputCSV, {
+    jsoncsv.buffered(finalSaleOrderItems, EXPORT_CSV_OPTIONS, (err, csv) => {
+        let out = fs.createWriteStream(PATH_OUTPUT_CSV, {
             encoding: 'utf8'
         })
         out.write(csv);
     })
 
 }
-let getIdsFromCSV = async () => {
+let GetIdsFromCSV = async () => {
     return await csv({
         noheader: true,
         fields: ["id"]
-    }).fromFile(pathInputCSV);
+    }).fromFile(PATH_INPUT_CSV);
 }
-
+let testIds = () => {
+    const start = 71045;
+    ids = []
+    for (let i = 0; i < 3; i++) {
+        ids.push({
+            "field1": (i + start)
+        });
+    }
+    return ids;
+}
 main()
